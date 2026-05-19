b573503645ebec298180d584dbba9d5586360fe2
yasir239 <yasoryhaaland@gmail.com>
2026-05-19 02:09:09 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     return jsonify({
         "status": "healthy",
-        "message": "Hello from AWS ECS Fargate! 🚀",
+        "message": "Hello from AWS ECS Fargate! ",
         "version": os.getenv("APP_VERSION", "1.0.0")
     })
 
